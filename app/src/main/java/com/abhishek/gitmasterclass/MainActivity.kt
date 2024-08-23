91d094ff4ae6ac87173d67b361de94ad9b4e08af
Abhishek Chandra <ac927920@gmail.com>
2024-08-23 12:56:53 +0530

    Basic Git Commands

--- app/src/main/java/com/abhishek/gitmasterclass/MainActivity.kt
+++ app/src/main/java/com/abhishek/gitmasterclass/MainActivity.kt
@@ -18,3 +18,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+/*
+    Git Course
+    git log = Showing all commits.
+    git status = Showing status of commits and working on project.
+    git add <filename> = Adding file to staging area.
+    git commit -m "commit message" = Committing changes to the repository.
+    git branch <branchname> = Creating a new branch.
+    git checkout <branchname> = Switching to a different branch.
+    git merge <branchname> = Merging a branch into current branch.
+
+    // Working Tree Clean Means not Changes on main branch to local branch.
+
+    git diff <branchname> = Show Differences between parents and children.
+*
+*
+*
+* */
